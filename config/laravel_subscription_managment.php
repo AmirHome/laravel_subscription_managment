43d08e8b4913053b25e623049855e2e13c112df8
AmirHome <amir.email@yahoo.com>
2025-10-24 12:41:10 +0300

    fix: Remove unused route_prefix configuration from subscription management

--- config/laravel_subscription_managment.php
+++ config/laravel_subscription_managment.php
@@ -3,7 +3,6 @@
 return [
     'path' => env('SUBSCRIPTION_PATH', 'subscriptions'),
     'middleware' => ['web'],
-    'route_prefix' => env('SUBSCRIPTION_ROUTE_PREFIX', ''),
     'table_prefix' => env('SUBSCRIPTION_PREFIX', 'saas_'),
     'model_path' => 'Amirhome\LaravelSubscriptionManagment\Models',
     // Number of grace period days added to end_at for validity checks
